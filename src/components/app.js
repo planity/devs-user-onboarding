0d6a092cc8c1f86f31289da8753b6e3f3a931c0e
VonD <paul.vonderscher@gmail.com>
2021-01-21 09:49:09 +0100

    adding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { colors, fontWeights } from 'config/styles';
+import { css } from 'glamor';
+
+css.global('*, *:before, *:after', {
+	boxSizing: 'border-box'
+});
+
+css.global('body', {
+	fontFamily: 'Open Sans, sans-serif'
+});
 
 export default function App() {
-	return <h1>Welcome to Planity !</h1>;
+	return <h1 css={styles.title}>Welcome to Planity !</h1>;
 }
+
+const styles = {
+	title: {
+		'margin': 10,
+		'color': colors.green500,
+		'fontWeight': fontWeights.bold,
+		'@media (min-width: 500px)': {
+			color: 'red'
+		}
+	}
+};
---
+++ src/config/styles.js
@@ -0,0 +1,7 @@
+export const colors = {
+	green500: '#48BB78'
+};
+
+export const fontWeights = {
+	bold: 'bold'
+};
